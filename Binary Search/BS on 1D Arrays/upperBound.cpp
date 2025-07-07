701bcbda2f9c14919e78e405b2b4fe74adb2bf69
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-07 17:47:25 +0530

    7 July 2025

---
+++ Binary Search/BS on 1D Arrays/upperBound.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+//time complexity - O(logn)
+
+//Iterative Approach
+int lowerBoundIterative(vector<int> &a, int n, int target){
+    int ans = n;
+    int low = 0;
+    int high = n-1;
+    while (low <= high){
+        int mid = (low + high)/2;
+        if(a[mid] > target){
+            ans = mid;
+            high = mid-1;
+        } 
+        else low = mid+1;
+    }
+    return ans;
+}
+
+//Recusive Approach
+int lowerBoundRecursive(vector<int> &a, int low, int high, int target){
+    int ans = a.size();
+    if(low > high) return ans;
+    int mid = (low + high)/2;
+    if(a[mid] > target){
+        ans = mid;
+        return min(mid,lowerBoundRecursive(a,low,mid-1,target));
+    }
+    else return lowerBoundRecursive(a,mid+1,high,target);
+    return ans;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    int target;
+    cin >> target;
+    //int index = lowerBoundIterative(a,n,target);
+    int index = lowerBoundRecursive(a,0,n-1,target);
+    cout << index;
+
+    //direct function
+    // auto ub = upper_bound(a.begin(),a.end(),target);
+    // if(ub != a.end()){
+    //     cout << ub - a.begin();
+    // }
+}
