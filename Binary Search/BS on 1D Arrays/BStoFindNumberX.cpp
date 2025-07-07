69c1b29e2222494aed5b12cbac75a3f0fd6b7853
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-07 17:07:50 +0530

    7 July 2025

---
+++ Binary Search/BS on 1D Arrays/BStoFindNumberX.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//only applicable in sorted search space
+//time complexity - O(logn)
+//overflow case if low = high = INT_MAX then mid = (INT_MAX+INT_MAX)/2 which exceeds int
+//use long long low,high or take mid = low + (high - low)/2
+
+//Iterative Approach
+int binarySearchIterative(vector<int> &a, int n, int target){
+    int low = 0;
+    int high = n-1;
+    while (low <= high){
+        int mid = (low + high)/2;
+        if(a[mid] == target) return mid;
+        else if(target > a[mid]) low = mid+1;
+        else high = mid-1;
+    }
+    return -1;
+}
+
+//Recusive Approach
+int binarySearchRecursive(vector<int> &a, int low, int high, int target){
+    if(low > high) return -1;
+    int mid = (low + high)/2;
+    if(a[mid] == target) return mid;
+    else if(target > a[mid]) return binarySearchRecursive(a,mid+1,high,target);
+    return binarySearchRecursive(a,low,mid,target);
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    int target;
+    cin >> target;
+    //int index = binarySearchIterative(a,n,target);
+    int index = binarySearchRecursive(a,0,n-1,target);
+    cout << index;
+}
---
+++ Binary Search/BS on 1D Arrays/lowerBound.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//only applicable in sorted search space
+//time complexity - O(logn)
+//overflow case if low = high = INT_MAX then mid = (INT_MAX+INT_MAX)/2 which exceeds int
+//use long long low,high or take mid = low + (high - low)/2
+
+//Iterative Approach
+int lowerBoundIterative(vector<int> &a, int n, int target){
+    int ans = n;
+    int low = 0;
+    int high = n-1;
+    while (low <= high){
+        int mid = (low + high)/2;
+        if(a[mid] >= target){
+            ans = mid;
+            high = mid-1;
+        } 
+        else low = mid+1;
+    }
+    return ans;
+}
+
+//Recusive Approach
+int binarySearchRecursive(vector<int> &a, int low, int high, int target){
+    int ans = a.size();
+    if(low > high) return ans;
+    int mid = (low + high)/2;
+    if(a[mid] >= target){
+        ans = mid;
+        binarySearchRecursive(a,low,mid,target);
+    }
+    else binarySearchRecursive(a,mid+1,high,target);
+    return ans;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    int target;
+    cin >> target;
+    //int index = lowerBoundIterative(a,n,target);
+    int index = binarySearchRecursive(a,0,n-1,target);
+    cout << index;
+}
