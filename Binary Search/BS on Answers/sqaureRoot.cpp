9f9540e27bdf7fff62859ebc754f109d3306b737
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-09 02:59:38 +0530

    9 July 2025

---
+++ Binary Search/BS on Answers/sqaureRoot.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+//brute force
+//O(n)
+int sqrt(int num){
+    int ans = 0;
+    for(int i = 0;i<=num;i++){
+        if(i*i<=num) ans = i;
+        else break;
+    }
+    return ans;
+}
+
+//optimal
+//O(logn)
+int sqrtOptimal(int num){
+    int ans = 0;
+    int low = 0;
+    int high = num-1;
+    while(low<=high){
+        long long mid = (low+high)/2;
+        if(mid*mid<=num){
+            ans = mid;
+            low = mid +1;
+        }
+        else if(mid*mid>num){
+            high = mid - 1;
+        }
+    }
+    return ans; //high will give the sqrt
+    //low will the give next value 
+}
+
+int main(){
+    int num;
+    cin >> num;
+    //int ans = sqrt(num);
+    int ans = sqrtOptimal(num);
+    cout << ans;
+}
