d4a45b0ce3c5009bce00019678a9777defddb105
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-17 13:50:52 +0530

    17 July 2025

---
+++ Binary Trees/Hard Problems/printRootToNodePath.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+bool rootToNodePathExist(Node* root,vector<int> &path, int target){
+    if(root==nullptr) return false;
+    path.push_back(root->data);
+    if(root->data==target){
+        return true;
+    }
+    if(rootToNodePathExist(root->left,path,target)||rootToNodePathExist(root->right,path,target)) return true;
+    path.pop_back();
+    return false;
+}
+
+vector<int> rootToNodePath(Node* root,int target){
+    vector<int> path;
+    if(root==nullptr) return path;
+    rootToNodePathExist(root,path,target);
+    return path;
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->left = new Node(6);
+    root->left->right->right = new Node(7);
+    int target = 7;
+    vector<int> ans = rootToNodePath(root,target);
+    for(auto it : ans){
+        cout << it << " ";
+    }
+}
