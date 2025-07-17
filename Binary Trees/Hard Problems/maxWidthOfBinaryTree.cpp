b90928e9210bc37c2250893bea62471c97d2e7e6
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-17 22:15:27 +0530

    17 July 2025

--- Binary Trees/Hard Problems/maxWidthOfBinaryTree.cpp
+++ Binary Trees/Hard Problems/maxWidthOfBinaryTree.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 struct Node{
@@ -12,15 +13,51 @@ struct Node{
     }
 };
 
-
+//optimal
+//TC O(n)
+//SC O(n)
+int maxWidth(Node* root){
+    if(root==nullptr) return 0;
+    int width = 0;
+    queue<pair<Node*,int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        int size = q.size();
+        int minn = q.front().second;
+        int first,last;
+        for(int i = 0;i<size;i++){
+            int curr = q.front().second - minn;
+            Node* node = q.front().first;
+            q.pop();
+            if(i==0) first = curr;
+            if(i==size-1) last = curr;
+            if(node->left!=nullptr){
+                q.push({node->left,2*curr + 1});
+            }
+            if(node->right!=nullptr){
+                 q.push({node->right,2*curr + 2});
+            }
+        }
+        width = max(width,last-first+1);
+    }
+    return width;
+}
 
 int main(){
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->left->right->left = new Node(6);
-    root->left->right->right = new Node(7);
-    
+    root->right->right = new Node(5);
+    int width = maxWidth(root);
+    cout << width;
 }
+
+//         1
+//       /   \
+//      /     \
+//     2       3
+//    /         \     
+//   /           \   
+//  4             5 
+// max width - 4
