209373df47240a091efad85c0096f271d6283399
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-19 00:21:28 +0530

    19 July 2025

--- Binary Trees/Hard Problems/constructBinaryTreeFromInorderAndPostordertraversal.cpp
+++ Binary Trees/Hard Problems/constructBinaryTreeFromInorderAndPostordertraversal.cpp
@@ -14,26 +14,26 @@ struct Node{
     }
 };
 
-Node* buildTree(vector<int> &preorder,int preStart,int preEnd,vector<int> &inorder,int inStart,int inEnd, map<int,int> &mpp){
-    if(preStart>preEnd || inStart>inEnd) return NULL;
-    Node* root = new Node(preorder[preStart]);
+Node* buildTree(vector<int> &postorder,int postStart,int postEnd,vector<int> &inorder,int inStart,int inEnd, map<int,int> &mpp){
+    if(postStart>postEnd || inStart>inEnd) return NULL;
+    Node* root = new Node(postorder[postEnd]);
     int inRoot = mpp[root->data];
     int numsLeft = inRoot-inStart;
-    root->left = buildTree(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,mpp);
-    root->right = buildTree(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,mpp);
+    root->left = buildTree(postorder,postStart,postStart+numsLeft-1,inorder,inStart,inRoot-1,mpp);
+    root->right = buildTree(postorder,postStart+numsLeft,postEnd-1,inorder,inRoot+1,inEnd,mpp);
     return root;
 }
 
 //optimal
 //TC O(n)
 //SC O(n) for map + O(H) auxillary stack space ~ O(n)
-Node* constructTree(vector<int> &preorder,vector<int> &inorder){
+Node* constructTree(vector<int> &postorder,vector<int> &inorder){
     map<int, int> mpp;
     for(int i = 0;i<inorder.size();i++){
         mpp[inorder[i]] = i;
     }
 
-    Node* root = buildTree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mpp);
+    Node* root = buildTree(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1,mpp);
     return root;
 
 }
@@ -47,8 +47,8 @@ void inOrder(Node* root){
 
 int main(){
     vector<int> inorder = {40,20,50,10,60,30};
-    vector<int> preorder = {10,20,40,50,30,60};
-    Node* root = constructTree(preorder,inorder);
+    vector<int> postorder = {40,50,20,60,30,10};
+    Node* root = constructTree(postorder,inorder);
     inOrder(root);
 }
 
