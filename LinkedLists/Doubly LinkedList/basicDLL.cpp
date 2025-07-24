4c2a83ec007caa6094b831d81bc487dbad1b0e4c
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-07-24 16:13:20 +0530

    24 July 2025

---
+++ LinkedLists/Doubly LinkedList/basicDLL.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{     //struct Node(use class than struct for OOPs)
+    public: 
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    public: //multiple constructors can be used
+    Node(int data1){
+        data = data1;
+        next = back = nullptr;
+    }
+};
+
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node x = Node(arr[0]);
+    cout << x.data << " " << x.next << " " << x.back << endl ;   //gives node value 
+    Node* y = new Node(arr[2]); //gives memory address
+    cout << y << endl;
+    cout << y->data;    //this will give data
+}
+
+//memory taken depends on system whether it is 32 bit or 64 bit
