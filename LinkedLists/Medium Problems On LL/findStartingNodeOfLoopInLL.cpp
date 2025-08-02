4470d7103b184072612f3155de81cb920cec9d87
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-02 13:51:07 +0530

    2 August 2025

---
+++ LinkedLists/Medium Problems On LL/findStartingNodeOfLoopInLL.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<unordered_map>
+using namespace std;
+
+class Node{    
+    public: 
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public: 
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* arrayToLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void traversalLL(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+//brute force
+//TC O(N*2*log(N))
+//SC O(N)
+Node* startingNodeOfCycle(Node *head){
+    Node* temp = head;
+    unordered_map<Node*, int> mpp;
+    while(temp!=nullptr){
+        if(mpp.find(temp)!=mpp.end()){
+            return temp;
+        }
+        mpp[temp] = 1;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+//optimal
+//TC O(N)
+//SC O(1)
+Node* startingNodeOfCycleOptimal(Node *head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){
+            slow = head;
+            while(slow!=fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+int main(){
+    Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+    Node* fifth = new Node(5);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = third; 
+    cout << "Starting Node Of Loop: ";
+    //Node* ans = startingNodeOfCycle(head);
+    Node* ans = startingNodeOfCycleOptimal(head);
+    cout << ans->data;
+}
