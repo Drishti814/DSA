3521422ec0c28eb859921236180a0f954ccf54ee
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-06 16:15:47 +0530

    6 Aug 2025

---
+++ Bit Manipulation/Learn Bit Manipulation/1sAnd2sComplement.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//TC O(len)
+//SC O(1)
+string onesComplement(string s){
+    string st = "";
+    int len = s.length();
+    for(int i = 0; i < len;i++){
+        if(s[i]=='1') st += '0';
+        else st+='1';
+    }
+    return st;
+}
+
+string twosComplement(string s){
+    string st = onesComplement(s);
+    int len = st.length();
+    for(int i = len-1; i>=0;i--){
+        if(st[i]=='0'){
+            st[i]='1';
+            break;
+        } 
+        else st[i] = '0';
+    }
+    if(st[0]=='0') return st;
+    else  return '1'+st;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    string one = onesComplement(s);
+    string two = twosComplement(s);
+    cout << "the 1s Complement of the string is:" << one << endl;
+    cout << "the 2s Complement of the string is:" << two;
+}
