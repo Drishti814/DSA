ed2c96da10d32a3dd25f06db5d9df8ecd75043df
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-15 23:49:39 +0530

    15 Aug 2025

---
+++ Bit Manipulation/Interview Problems/singleNumberI.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+#include<vector>
+#include<map>
+using namespace std;
+
+//brute force
+//TC O(n*logm +m) n = nums.size(),m = map.size()
+//SC O(m)
+int singleNumber(vector<int> &nums){
+    map<int,int> mpp;
+    for(int i = 0;i <nums.size();i++){
+        mpp[nums[i]]++;
+    }
+    for(auto it:mpp){
+        if(it.second==1){
+            return it.first;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    int ans = singleNumber(nums);
+    cout << ans;
+}
