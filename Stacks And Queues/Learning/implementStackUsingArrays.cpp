ccc59ddca5bc795e74bd47416fbbb549aaffea37
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-21 00:36:52 +0530

    21 Aug 2025

---
+++ Stacks And Queues/Learning/implementStackUsingArrays.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+
+    int size;
+    int * arr;
+    int top;
+
+    public:
+        Stack(){
+            top = -1;
+            size = 10;
+            arr = new int[size];
+        }
+
+    void push(int x){
+        if(top==size-1){
+            cout << " Stack Overflow";
+            return;
+        }
+        top++;
+        arr[top] = x;
+    }
+
+    int Top(){
+        if(top==-1){
+            cout << " Stack empty";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    int pop(){
+        if(top==-1){
+            cout << " Stack Underflow";
+            return -1;
+        }
+        int el = arr[top];
+        top--;
+        return el;
+    }
+
+    int Size(){
+        return top+1;
+    }
+};
+
+int main(){
+    Stack s;
+    s.push(6);
+    s.push(3);
+    s.push(7);
+    cout << "Top of stack is before deleting any element " << s.Top() << endl;
+    cout << "Size of stack before deleting any element " << s.Size() << endl;
+    cout << "The element deleted is " << s.pop() << endl;
+    cout << "Size of stack after deleting an element " << s.Size() << endl;
+    cout << "Top of stack after deleting an element " << s.Top() << endl;
+    return 0;
+}
