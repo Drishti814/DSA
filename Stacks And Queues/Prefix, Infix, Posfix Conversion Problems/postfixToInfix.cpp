42986fd493595efc7c7e77b632ff971dc7f607de
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-23 11:27:01 +0530

    23 Aug 2025

---
+++ Stacks And Queues/Prefix, Infix, Posfix Conversion Problems/postfixToInfix.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<climits>
+#include<stack>
+using namespace std;
+
+int priority(char ch){
+    if(ch=='^') return 3;
+    else if(ch=='*' || ch=='/') return 2;
+    else if(ch=='+' || ch=='-') return 1;
+    return -1;
+}
+
+//TC O(N+N)
+//SC O(N)
+void postfixToInfix(string s){
+    int n = s.length();
+    int i = 0;
+    stack<string> st;
+    string ans = "";
+    while(i<n){
+        if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9')){
+            st.push(string(1,s[i]));
+        }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string exp = "("+t2+string(1,s[i])+t1+")";
+            st.push(exp);
+        }
+        i++;
+    }
+    cout << "Infix expression: " << st.top() << endl;
+}
+
+int main(){
+    string exp = "ab-de+f*/";
+    cout << "Postfix expression: " << exp << endl;
+    postfixToInfix(exp);
+}
