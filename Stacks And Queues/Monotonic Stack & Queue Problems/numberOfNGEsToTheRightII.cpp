27137a185042663252d6b98dfccb69e3aa14228b
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-23 17:04:33 +0530

    23 Aug 2025

--- Stacks And Queues/Monotonic Stack & Queue Problems/numberOfNGEsToTheRightII.cpp
+++ Stacks And Queues/Monotonic Stack & Queue Problems/numberOfNGEsToTheRightII.cpp
@@ -4,25 +4,25 @@
 #include<stack>
 using namespace std;
 
-//brute force
-//TC O(N^2)
-//SC O(N)
+//another meaning of NGEs to right is Take each element → count its first NGE → then for that NGE count its NGE → continue until no NGE → total = number of NGEs in this chain.
+
+//TC O(2N)
+//SC O(2N)
 vector<int> numOfNGEsToTheRight(vector<int> & arr){
     int n = arr.size();
     vector<int> ans(n,0);
-    for(int i = 0;i<n;i++){
-        for(int j = i+1;j<n;j++){
-            if(arr[i]<arr[j]){
-                ans[i]++;
-                
-            }
+    stack<int> st;
+    int i = n-1;
+    while(i>=0){
+        while(!st.empty() && st.top() <= arr[i]){
+            st.pop();
         }
+        ans[i]=st.size();
+        st.push(arr[i]);
+        i--;
     }
     return ans;
 }
-
-//optimal is using fenwick Tree (don't know how to do that)
-//TC O(NlogN)
 
 int main(){
     vector<int> arr = {6,0,8,1,3};
