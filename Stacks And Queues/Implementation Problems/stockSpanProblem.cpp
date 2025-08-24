efd31ea1bf9387950d39a9ecfea6cee54f7916bd
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-25 00:43:23 +0530

    25 Aug 2025

---
+++ Stacks And Queues/Implementation Problems/stockSpanProblem.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+//brute force
+//TC O(number of days)
+//SC O(total number of next calls)
+class StockSpanner {
+public:
+    vector<int> arr;
+    int cnt;
+    StockSpanner() {
+        arr = {};
+    }
+    
+    int next(int price) {
+        arr.push_back(price);
+        cnt = 1;
+        for(int i = arr.size()-2;i>=0;i--){
+            if(arr[i]<=price) cnt++;
+            else break;
+        }
+        return cnt;
+    }
+};
+
+//optimal
+class StockSpannerOptimal {
+public:
+    stack<pair<int,int>> st;
+    int ind;
+    int ans;
+    StockSpannerOptimal() {
+        st.empty();
+        ind = -1;
+        ans = 1;
+    }
+    
+    int next(int price) {
+        ind = ind+1;
+        while(!st.empty() && st.top().first<=price){
+            st.pop();
+        }
+        ans = ind - (st.empty()?-1:st.top().second);
+        st.push({price,ind});
+        return ans;
+    }
+};
+
+int main(){
+    //StockSpanner obj;
+    StockSpannerOptimal obj;
+    vector<int> prices = {100, 80, 60, 70, 60, 75, 85};
+    for (int price : prices) {
+        cout << "next(" << price << ") -> " << obj.next(price) << endl;
+    }
+}
