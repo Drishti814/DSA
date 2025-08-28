c0786882cb2ccfc7908d87bd101031082f1cbf99
Drishti <148680551+Drishti814@users.noreply.github.com>
2025-08-28 10:03:16 +0530

    28 Aug 2025

--- Greedy Algorithms/Medium&Hard/LRUPageReplacement.cpp
+++ Greedy Algorithms/Medium&Hard/LRUPageReplacement.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include<vector>
 #include<algorithm>
+#include<climits>
+#include<queue>
 #include<unordered_map>
 using namespace std;
 
-void LRUPageReplacement(vector<int>& pages, int capacity) {
+//brute force
+//TC O(n * capacity)
+//SC O(N+capacity)
+void LRUPageReplacementBrute(vector<int>& pages, int capacity) {
     int n = pages.size();
     vector<int> frames;
     unordered_map<int,int> lastUsed;
@@ -41,7 +46,10 @@ void LRUPageReplacement(vector<int>& pages, int capacity) {
     cout << "Total Page Faults = " << faults << endl;
 }
 
-void LRUPageReplacementOptimal(vector<int>& pages, int capacity) {
+//Belady’s Optimal Algorithm(optimal because it gives minimum page faults not computationally optimal)
+//TC O(n^2 * capacity)
+//SC O(capacity)
+void LRUPageReplacement(vector<int>& pages, int capacity) {
     int n = pages.size();
     vector<int> frames;
     int faults = 0;
@@ -84,9 +92,80 @@ void LRUPageReplacementOptimal(vector<int>& pages, int capacity) {
     cout << "Total Page Faults = " << faults << endl;
 }
 
+struct Frame{
+    int page;
+    int nextUse;
+    bool operator< (const Frame &other) const{
+        return nextUse<other.nextUse;
+    }
+};
+
+//optimal
+//TC O(N * log(capacity))
+//SC O(N+capacity)
+void LRUPageReplacementOptimal(vector<int>& pages, int capacity) {
+    int n = pages.size();
+    unordered_map<int,queue<int>> future;
+    for(int i = 0;i<n;i++){
+        future[pages[i]].push(i);
+    }
+    vector<int> frames;
+    unordered_map<int, int> inFrame;    
+    priority_queue<Frame> pq;  
+    int faults = 0;
+
+    for(int i = 0;i<n;i++){
+        int page = pages[i];
+        future[page].pop();
+
+        int nextUse = (future[page].empty()?INT_MAX:future[page].front());
+
+        if(inFrame.find(page) != inFrame.end()){
+            inFrame[page] = nextUse;
+            pq.push({page,nextUse});
+        }
+        else{
+            faults++;
+            if(frames.size()<capacity){
+                frames.push_back(page);
+                inFrame[page]=nextUse;
+                pq.push({page,nextUse});
+            }
+            else{
+                while(!pq.empty()){
+                    Frame top = pq.top();
+                    if(inFrame[top.page]!=top.nextUse){
+                        pq.pop();
+                    }
+                    else break;
+                }
+            }
+            Frame victim = pq.top(); 
+            pq.pop();
+            int evictPage = victim.page;
+            for (int &f : frames) {
+                if (f == evictPage) {
+                    f = page;
+                    break;
+                }
+            }
+            inFrame.erase(evictPage);
+            inFrame[page] = nextUse;
+            pq.push({page, nextUse});
+        }
+        cout << "Step " << i+1 << " (page=" << page << "): ";
+        for (int f : frames) cout << f << " ";
+        cout << endl;
+    }
+    cout << "Total Page Faults = " << faults << endl;
+}
+
 int main() {
     vector<int> pages = {1,2,3,4,1,2,5,1,2,3,4,5};
+    //LRUPageReplacementBrute(pages, 3);
     //LRUPageReplacement(pages, 3);
     LRUPageReplacementOptimal(pages, 3);
     return 0;
 }
+
+
